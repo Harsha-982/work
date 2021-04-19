437bed1c7ee9b023570d370b882fdbab8e6ae08f
user <user@192.168.0.8>
2021-04-19 19:38:37 +0530

    19/04/2021

--- Wrapper/src/Wrapper/ToString.java
+++ Wrapper/src/Wrapper/ToString.java
@@ -5,12 +5,12 @@ public class ToString {
 	public static void main(String[] args) {
 		int a=10;
 		Integer I =Integer.valueOf(a);
-		String strInteger= I.toString();
-		String strPrimitive= Integer.toString(a);
+		String strInteger= I.toString(); // converted to string class from Integer type
+		String strPrimitive= Integer.toString(a); // converted to string from Primitive type
 		
 		// to check whether converted to string or not 
 		
-		System.out.println(strInteger.getClass().getSimpleName()); // converted to string class from Integer type
-		System.out.println(strPrimitive.getClass().getSimpleName()); // converted to string from Primitive type
+		System.out.println(strInteger.getClass().getSimpleName()); 
+		System.out.println(strPrimitive.getClass().getSimpleName());
 	} 
 }

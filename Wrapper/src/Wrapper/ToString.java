f49537afc85396bbd637cc8b40d9c0ecf0817420
Harsha-982 <82398710+Harsha-982@users.noreply.github.com>
2021-04-19 19:52:19 +0530

    Update ToString.java

--- Wrapper/src/Wrapper/ToString.java
+++ Wrapper/src/Wrapper/ToString.java
@@ -6,11 +6,13 @@ public class ToString {
 		int a=10;
 		Integer I =Integer.valueOf(a);
 		String strInteger= I.toString(); // converted to string class from Integer type
-		String strPrimitive= Integer.toString(a); // converted to string from Primitive type
+		String strPrimitive= Integer.toString(a,3); // converted to string from Primitive type
 		
 		// to check whether converted to string or not 
 		
 		System.out.println(strInteger.getClass().getSimpleName()); 
 		System.out.println(strPrimitive.getClass().getSimpleName());
+		// converting to base3 string.. only in integer and long 
+		System.out.println(strPrimitive);
 	} 
 }

fabe1f974e3ce46a1c18d078a8747bc93cc4f71e
Harsha-982 <82398710+Harsha-982@users.noreply.github.com>
2021-04-20 20:26:55 +0530

    Update Reverse.java

--- StringReverse/src/Reverse/Reverse.java
+++ StringReverse/src/Reverse/Reverse.java
@@ -7,12 +7,12 @@ public class Reverse {
 		Scanner sc =new Scanner(System.in);
 		
 // 		String string= sc.nextLine();
+// 		String s=" ";
 		StringBuffer sb= new StringBuffer(sc.nextLine());
-		System.out.println(sb.reverse())
-// 		String s="";
-// 		for (int i=string.length()-1;i>=0;i--) {
-// 			s=s+string.charAt(i);		
+		StringBuffer sb = new StringBuffer();
+		for (int i=string.length()-1;i>=0;i--) {
+			sb.append(string.charAt(i));
 		}
-		System.out.println(s);
+		System.out.println(sb);
 	}
 }

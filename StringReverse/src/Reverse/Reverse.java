79b184ed3d4ee417b5655bd021598d1d306d563b
Harsha-982 <82398710+Harsha-982@users.noreply.github.com>
2021-04-20 10:48:53 +0530

    Update Reverse.java

--- StringReverse/src/Reverse/Reverse.java
+++ StringReverse/src/Reverse/Reverse.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 public class Reverse {
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
-		String string= sc.nextLine();
-		String s="";
-		for (int i=string.length()-1;i>0;i--) {
-			s=s+string.charAt(i);		
+		
+// 		String string= sc.nextLine();
+		StringBuffer sb= new StringBuffer(sc.nextLine());
+		System.out.println(sb.reverse())
+// 		String s="";
+// 		for (int i=string.length()-1;i>0;i--) {
+// 			s=s+string.charAt(i);		
 		}
 		System.out.println(s);
 	}
